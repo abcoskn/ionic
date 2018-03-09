395d90ca58fa8ef7ea407566d78ab31d8a88e432
Abdullah Yaşar COŞKUN <ugur.bekdas@yrnet.com>
2018-03-09 16:24:37 +0300

    stores map test release

--- src/pages/stores-map/stores-map.ts
+++ src/pages/stores-map/stores-map.ts
@@ -47,10 +47,10 @@ mypoint:any;
 
   showMap(){
 
-    const location = new google.maps.LatLng(38.7270367398, 35.5181121826);
+    const location = new google.maps.LatLng(39.564056, 34.506425);
     const options={
       center:location,
-      zoom:30
+      zoom:5
     };
     this.map=new google.maps.Map(this.mapRef.nativeElement,options);
     
@@ -64,6 +64,7 @@ mypoint:any;
         icon:mypin
       });
       this.map.setCenter(new google.maps.LatLng(resp.coords.latitude,resp.coords.longitude));
+      this.map.setZoom(16);
       let watch = this.geolocation.watchPosition();
       watch.subscribe((data) => {
         var latlng = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
@@ -111,5 +112,66 @@ mypoint:any;
   closewindow(){
     document.getElementById("map").style.height="100%";
   }
+  addYourLocationButton(map, marker) 
+  {
+      var controlDiv = document.createElement('div');
   
+      var firstChild = document.createElement('button');
+      firstChild.style.backgroundColor = '#fff';
+      firstChild.style.border = 'none';
+      firstChild.style.outline = 'none';
+      firstChild.style.width = '28px';
+      firstChild.style.height = '28px';
+      firstChild.style.borderRadius = '2px';
+      firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+      firstChild.style.cursor = 'pointer';
+      firstChild.style.marginRight = '10px';
+      firstChild.style.padding = '0px';
+      firstChild.title = 'Your Location';
+      controlDiv.appendChild(firstChild);
+  
+      var secondChild = document.createElement('div');
+      secondChild.style.margin = '5px';
+      secondChild.style.width = '18px';
+      secondChild.style.height = '18px';
+      secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-1x.png)';
+      secondChild.style.backgroundSize = '180px 18px';
+      secondChild.style.backgroundPosition = '0px 0px';
+      secondChild.style.backgroundRepeat = 'no-repeat';
+      secondChild.id = 'you_location_img';
+      firstChild.appendChild(secondChild);
+  
+      google.maps.event.addListener(map, 'dragend', function() {
+          //$('#you_location_img').css('background-position', '0px 0px');
+          document.getElementById("you_location_img").style.backgroundPosition="0px 0px";
+      });
+  
+      firstChild.addEventListener('click', function() {
+          var imgX = '0';
+          var animationInterval = setInterval(function(){
+              if(imgX == '-18') imgX = '0';
+              else imgX = '-18';
+              //$('#you_location_img').css('background-position', imgX+'px 0px');
+              document.getElementById("you_location_img").style.backgroundPosition=imgX+"px 0px";
+          }, 500);
+          if(navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                  marker.setPosition(latlng);
+                  map.setCenter(latlng);
+                  clearInterval(animationInterval);
+                  //$('#you_location_img').css('background-position', '-144px 0px');
+                  document.getElementById("you_location_img").style.backgroundPosition="-144px 0px";
+              });
+          }
+          else{
+              clearInterval(animationInterval);
+              //$('#you_location_img').css('background-position', '0px 0px');
+              document.getElementById("you_location_img").style.backgroundPosition="0px 0px";
+          }
+      });
+  
+      controlDiv.tabIndex = 1;
+      map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+  }
 }
