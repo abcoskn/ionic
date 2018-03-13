453f0138bfff0ace32c92b4486b52eba9a5f5552
Abdullah Yaşar COŞKUN <ugur.bekdas@yrnet.com>
2018-03-13 18:01:47 +0300

    hand on yrcard

--- src/pages/yrcard/yrcard.ts
+++ src/pages/yrcard/yrcard.ts
@@ -17,18 +17,30 @@ export class YrcardPage {
 task1:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.task1=setTimeout(()=>{
-      if(document.getElementById("card").className=="")
-        document.getElementById("card").className="flipped";
-      else
-        document.getElementById("card").className="";
+      document.getElementById("hand").style.zoom="0.8";
+    },800);
+    this.task1=setTimeout(()=>{
+      document.getElementById("hand").style.zoom="1";
     },1000);
     this.task1=setTimeout(()=>{
-      if(document.getElementById("card").className=="")
-        document.getElementById("card").className="flipped";
-      else
-        document.getElementById("card").className="";
-    },2000);
+      this.flip();
+    },1200);
+
+
+    this.task1=setTimeout(()=>{
+      document.getElementById("hand").style.zoom="0.8";
+    },2500);
+    this.task1=setTimeout(()=>{
+      document.getElementById("hand").style.zoom="1";
+    },2700);
+    this.task1=setTimeout(()=>{
+      this.flip();
+    },2900);
+    this.task1=setTimeout(()=>{
+      document.getElementById("hand").style.top="-300px";
+    },3900);
   }
+
   flip(){
     if(document.getElementById("card").className=="")
       document.getElementById("card").className="flipped";
